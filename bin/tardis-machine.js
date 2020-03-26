f63a9eb0e7698090c3f3f45062d71c8e417bea64
Thad <thad@tardis.dev>
2020-03-26 10:56:41 +0100

    chore: log process unhandledRejection uncaughtException errors to strerr

--- bin/tardis-machine.js
+++ bin/tardis-machine.js
@@ -108,3 +108,12 @@ async function start() {
 }
 
 start()
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error('Unhandled Rejection at Promise', reason, p)
+  })
+  .on('uncaughtException', (err) => {
+    console.error('Uncaught Exception thrown', err)
+    process.exit(1)
+  })
